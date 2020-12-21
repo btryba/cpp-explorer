d1d2d4bbbf5dfbcc060b6c5f84e2bf3623ff6a8c
btryba <btryba@gmail.com>
2020-12-20 21:09:41 -0600

    Broke out file data into separate class.

--- src/FileSystemInterface.ts
+++ src/FileSystemInterface.ts
@@ -4,14 +4,9 @@ import * as fs from 'fs';
 import * as readline from 'readline';
 import { TreeNodeType } from './TreeNode';
 
-export class FileSystemInterface
+class FileData
 {
-    constructor(private workspaceRoot: any)
-    {
-        this.workspaceRoot = workspaceRoot;
-    }
-
-    createHeaderFile(fullPath:string, projectName: string, className: string)
+    static headerFile(projectName: string, className: string) : string
     {
         var fileContents = "#ifndef "+projectName.toUpperCase()+"_HPP\n"
         + "#define "+projectName.toUpperCase()+"_HPP\n\n"
@@ -27,23 +22,22 @@ export class FileSystemInterface
         }
         fileContents += "}\n\n"
         + "#endif //"+projectName.toUpperCase()+"_HPP";
-        fs.writeFileSync(fullPath, fileContents);
+        return fileContents;
     }
 
-    createTemplateFile(fullPath:string, projectName: string, className: string)
+    static templateFile(projectName: string, className: string) :string
     {
-        var fileContents = "#ifndef "+projectName.toUpperCase()+"_HPP\n"
+        return "#ifndef "+projectName.toUpperCase()+"_HPP\n"
         + "#define "+projectName.toUpperCase()+"_HPP\n\n"
         + "namespace "+projectName+"\n"
-        + "{\n";
-        + "    template <>\n";
+        + "{\n"
+        + "    template <>\n"
         + "    class "+className+"\n    {\n\n    };\n"
         + "}\n\n"
         + "#endif //"+projectName.toUpperCase()+"_HPP";
-        fs.writeFileSync(fullPath, fileContents);
     }
 
-    createImplementationFile(fullPath:string, projectName: string, className :string)
+    static implementationFile(projectName: string, className: string) : string
     {
         var fileContents = "";
         if (className !== "")
@@ -53,7 +47,175 @@ export class FileSystemInterface
         fileContents += "namespace "+projectName+"\n"
         + "{\n\n"
         + "}\n";
-        fs.writeFileSync(fullPath, fileContents);
+        return fileContents;
+    }
+
+    static internalHeader(projectName: string) : string
+    {
+        return "#ifndef INTERNALKEYWORD_HPP\n"
+        + "    #define INTERNALKEYWORD_HPP\n\n"
+        + "    #ifndef "+projectName.toUpperCase()+"INTERNAL\n"
+        + "        #define _internal public\n"
+        + "    #else\n"
+        + "        #define _internal private\n"
+        + "    #endif\n"
+        + "#endif\n";
+    }
+
+    static combinedHeader(projectName: string, list: string[], lengthToClip: number, internalkey: boolean) : string
+    {
+        var capitalProjectName = projectName.toUpperCase();
+        var fileContents = "#ifndef " + capitalProjectName + "_HPP\n"
+        + "#define " + capitalProjectName + "_HPP\n\n";
+        if(internalkey)
+        {
+            fileContents += "    #undef _internal\n";   
+            fileContents += "    #define " + capitalProjectName + "INTERNAL\n\n";   
+        }
+        var includeLoop;
+        for(includeLoop = 0; includeLoop < list.length; includeLoop++)
+        {
+            var shortenedName = list[includeLoop].substring(lengthToClip);
+            fileContents += "    #include \"" + shortenedName + "\"\n";
+        }
+        fileContents += "\n";
+        if(internalkey)
+        {
+            fileContents += "    #undef _internal\n";   
+            fileContents += "    #undef " + capitalProjectName + "INTERNAL\n\n";   
+        }
+        fileContents += "#endif //" + capitalProjectName + "_HPP\n";
+        return fileContents;
+    }
+
+    static projectConfig(projectType: TreeNodeType) : string
+    {
+        var fileContents = "include(CppExplorerOptions.cmake)\n"
+        + "cmake_minimum_required(VERSION ${MinimumCMakeVersion})\n"
+        + "project(${ProjectName} VERSION 0.1.0)\n\n"
+        + "include_directories(\"include\")\n"
+        + "include_directories(\"..\")\n\n"
+        + "include_directories(\"../libraries\")\n\n"
+        + "#Add all the source files to build library\n"
+        + "include(\"${CMAKE_CURRENT_SOURCE_DIR}/SourceFiles.cmake\")\n\n";
+        
+        if(projectType === TreeNodeType.executable)
+        {
+            fileContents += "add_executable(${PROJECT_NAME} ${SourceFiles})\n\n";
+        }
+        else
+        {
+            fileContents += "add_library(${PROJECT_NAME} SHARED ${SourceFiles})\n"
+            + "add_library(${PROJECT_NAME}_STATIC STATIC ${SourceFiles})\n\n";
+        }
+    
+        fileContents += "if(${CMAKE_SYSTEM_NAME} STREQUAL \"Windows\")\n"
+        + "    if(NOT ${WindowsPreBuildCommand} STREQUAL \"\")\n"
+        + "        add_custom_command(TARGET ${PROJECT_NAME}\n"
+        + "            PRE_BUILD COMMAND\n"
+        + "            ${WindowsPreBuildCommand}\n"
+        + "        )\n"
+        + "    endif()\n"
+        + "    if(NOT ${WindowsPostBuildCommand} STREQUAL \"\")\n"
+        + "        add_custom_command(TARGET ${PROJECT_NAME}\n"
+        + "            POST_BUILD COMMAND\n"
+        + "            ${WindowsPostBuildCommand}\n"
+        + "        )\n"
+        + "    endif()\n"
+        + "else()\n"
+        + "    if(NOT ${UnixPreBuildCommand} STREQUAL \"\")\n"
+        + "        add_custom_command(TARGET ${PROJECT_NAME}\n"
+        + "            PRE_BUILD COMMAND\n"
+        + "            ${UnixPreBuildCommand}\n"
+        + "        )\n"
+        + "    endif()\n"
+        + "    if(NOT ${UnixPostBuildCommand} STREQUAL \"\")\n"
+        + "        add_custom_command(TARGET ${PROJECT_NAME}\n"
+        + "            POST_BUILD COMMAND\n"
+        + "            ${UnixPostBuildCommand}\n"
+        + "        )\n"
+        + "    endif()\n"
+        + "endif()";
+        return fileContents;
+    }
+
+    static mainCpp() : string
+    {
+        return "int main(int argCount, char *argValues[])\n"
+        + "{\n"
+        + "\n"
+        + "    return 0;\n"
+        + "}";
+    }
+
+    static projectOptions(projectName: string, projectType: TreeNodeType) : string
+    {
+        var fileContents = "set(ProjectName "+projectName+")\n"
+        + "set(ProjectVersion 1.0.0)\n"
+        + "OPTION(EnableTesting \"Turn on Testing\" ON)\n";
+        + "OPTION(EnableInternalKeyword \"Creates an _internal keyword for more access control\" ON)\n";
+        if(projectType === TreeNodeType.executable)
+        {
+            fileContents += "OPTION(AutoGenCombinedLibraryHeader \"For library projects, create a combined header\" OFF)\n";
+        }
+        else
+        {
+            fileContents += "OPTION(AutoGenCombinedLibraryHeader \"For library projects, create a combined header\" ON)\n";
+        }
+        fileContents += "\n"
+        + "## Symbol && can be used to run multiple commands ##\n"
+        + "set(WindowsPreBuildCommand \"\")\n"
+        + "set(WindowsPostBuildCommand \"\")\n"
+        + "set(UnixPreBuildCommand \"\")\n"
+        + "set(UnixPostBuildCommand \"\")\n";
+        return fileContents;
+    }
+
+    static workspaceConfig() : string
+    {
+        return "include(CppExplorerOptions.cmake)\n"
+        + "cmake_minimum_required(VERSION ${MinimumCMakeVersion})\n"
+        + "project("+vscode.workspace.name+" VERSION 0.1.0)\n\n"
+        + "include(CppExplorerDependancies.cmake)\n\n"
+        + "if(${EnableTesting})\n"
+        + "    include(CTest)\n"
+        + "    enable_testing()\n"
+        + "endif()\n\n"
+        + "#Determine if 32 or 64 bit\n"
+        + "set(OSBitness 32)\n"
+        + "if(CMAKE_SIZEOF_VOID_P EQUAL 8)\n"
+        + "    set(OSBitness 64)\n"
+        + "endif()\n\n"
+        + "#Save outputs into bin folder\n"
+        + "set(FullOutputDir \"${CMAKE_SOURCE_DIR}/bin/${CMAKE_SYSTEM_NAME}${OSBitness}/${CMAKE_BUILD_TYPE}\")\n"
+        + "set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY \"${FullOutputDir}/static libs\")\n"
+        + "set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${FullOutputDir})\n"
+        + "set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${FullOutputDir})\n"
+        + "set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${FullOutputDir})\n\n"
+        + "include(CppExplorerProjects.cmake)";
+    }
+}
+
+export class FileSystemInterface
+{
+    constructor(private workspaceRoot: any)
+    {
+        this.workspaceRoot = workspaceRoot;
+    }
+
+    createHeaderFile(fullPath:string, projectName: string, className: string)
+    {
+        fs.writeFileSync(fullPath, FileData.headerFile(projectName, className));
+    }
+
+    createTemplateFile(fullPath:string, projectName: string, className: string)
+    {
+        fs.writeFileSync(fullPath, FileData.templateFile(fullPath, className));
+    }
+
+    createImplementationFile(fullPath:string, projectName: string, className :string)
+    {
+        fs.writeFileSync(fullPath, FileData.implementationFile(projectName, className));
     }
 
     generateInternalHeader(projectName :string)
@@ -61,15 +223,7 @@ export class FileSystemInterface
         var internalPath = this.workspaceRoot+"/"+projectName+"/include/InternalKeyword.hpp";
         if(!this.pathExists(internalPath))
         {
-            fs.writeFileSync(internalPath,
-            "#ifndef INTERNALKEYWORD_HPP\n"
-            + "    #define INTERNALKEYWORD_HPP\n\n"
-            + "    #ifndef "+projectName.toUpperCase()+"INTERNAL\n"
-            + "        #define _internal public\n"
-            + "    #else\n"
-            + "        #define _internal private\n"
-            + "    #endif\n"
-            + "#endif\n");
+            fs.writeFileSync(internalPath, FileData.internalHeader(projectName));
         }
     }
     
@@ -78,26 +232,8 @@ export class FileSystemInterface
         var internalkey = this.getOption("EnableInternalKeyword", projectName);
         var list :string[]= [];
         list = this.makeListFiles(this.workspaceRoot+"/"+projectName, list, ".hpp");
-        var fileContents = "#ifndef "+projectName.toUpperCase()+"_HPP\n"
-        + "#define "+projectName.toUpperCase()+"_HPP\n\n";
-        if(internalkey)
-        {
-            fileContents += "    #undef _internal\n";   
-            fileContents += "    #define "+projectName.toUpperCase()+"INTERNAL\n\n";   
-        }
-        var includeLoop;
-        for(includeLoop = 0; includeLoop < list.length; includeLoop++)
-        {
-            var shortenedName = list[includeLoop].substring(this.workspaceRoot.length+projectName.length+2);
-            fileContents += "    #include \""+shortenedName+"\"\n";
-        }
-        fileContents += "\n";
-        if(internalkey)
-        {
-            fileContents += "    #undef _internal\n";   
-            fileContents += "    #undef "+projectName.toUpperCase()+"INTERNAL\n\n";   
-        }
-        fileContents += "#endif //"+projectName.toUpperCase()+"_HPP\n";
+        var pathRootLength = this.workspaceRoot.length+projectName.length+2;
+        var fileContents = FileData.combinedHeader(projectName, list, pathRootLength, internalkey);
         this.writeFile(projectName+"/"+projectName+".hpp",fileContents);
     }
 
@@ -298,80 +434,15 @@ export class FileSystemInterface
         this.createPath(projectName+"/src");
     
         fs.writeFile(projectRoot+"/SourceFiles.cmake","", (err) =>{});
-                
-        let fileContents = "include(CppExplorerOptions.cmake)\n"
-        + "cmake_minimum_required(VERSION ${MinimumCMakeVersion})\n"
-        + "project(${ProjectName} VERSION 0.1.0)\n\n"
-        + "include_directories(\"include\")\n"
-        + "include_directories(\"..\")\n\n"
-        + "include_directories(\"../libraries\")\n\n"
-        + "#Add all the source files to build library\n"
-        + "include(\"${CMAKE_CURRENT_SOURCE_DIR}/SourceFiles.cmake\")\n\n";
-        
-        if(projectType === TreeNodeType.executable)
-        {
-            fileContents += "add_executable(${PROJECT_NAME} ${SourceFiles})\n\n";
-        }
-        else
-        {
-            fileContents += "add_library(${PROJECT_NAME} SHARED ${SourceFiles})\n"
-            + "add_library(${PROJECT_NAME}_STATIC STATIC ${SourceFiles})\n\n";
-        }
-    
-        fileContents += "if(${CMAKE_SYSTEM_NAME} STREQUAL \"Windows\")\n"
-        + "    if(NOT ${WindowsPreBuildCommand} STREQUAL \"\")\n"
-        + "        add_custom_command(TARGET ${PROJECT_NAME}\n"
-        + "            PRE_BUILD COMMAND\n"
-        + "            ${WindowsPreBuildCommand}\n"
-        + "        )\n"
-        + "    endif()\n"
-        + "    if(NOT ${WindowsPostBuildCommand} STREQUAL \"\")\n"
-        + "        add_custom_command(TARGET ${PROJECT_NAME}\n"
-        + "            POST_BUILD COMMAND\n"
-        + "            ${WindowsPostBuildCommand}\n"
-        + "        )\n"
-        + "    endif()\n"
-        + "else()\n"
-        + "    if(NOT ${UnixPreBuildCommand} STREQUAL \"\")\n"
-        + "        add_custom_command(TARGET ${PROJECT_NAME}\n"
-        + "            PRE_BUILD COMMAND\n"
-        + "            ${UnixPreBuildCommand}\n"
-        + "        )\n"
-        + "    endif()\n"
-        + "    if(NOT ${UnixPostBuildCommand} STREQUAL \"\")\n"
-        + "        add_custom_command(TARGET ${PROJECT_NAME}\n"
-        + "            POST_BUILD COMMAND\n"
-        + "            ${UnixPostBuildCommand}\n"
-        + "        )\n"
-        + "    endif()\n"
-        + "endif()";
 
-        this.writeFile(projectName+"/CMakeLists.txt", fileContents);
+        this.writeFile(projectName+"/CMakeLists.txt", FileData.projectConfig(projectType));
   
         if(projectType === TreeNodeType.executable)
         {
-            fs.writeFile(projectRoot+"/main.cpp","int main(int argCount, char *argValues[])\n{\n\n    return 0;\n}", (err) =>{});
+            fs.writeFile(projectRoot+"/main.cpp", FileData.mainCpp(), (err) =>{});
         }
-  
-        fileContents = "set(ProjectName "+projectName+")\n"
-        + "set(ProjectVersion 1.0.0)\n"
-        + "OPTION(EnableTesting \"Turn on Testing\" ON)\n";
-        + "OPTION(EnableInternalKeyword \"Creates an _internal keyword for more access control\" ON)\n";
-        if(projectType === TreeNodeType.executable)
-        {
-            fileContents += "OPTION(AutoGenCombinedLibraryHeader \"For library projects, create a combined header\" OFF)\n";
-        }
-        else
-        {
-            fileContents += "OPTION(AutoGenCombinedLibraryHeader \"For library projects, create a combined header\" ON)\n";
-        }
-        fileContents += "\n"
-        + "## Symbol && can be used to run multiple commands ##\n"
-        + "set(WindowsPreBuildCommand \"\")\n"
-        + "set(WindowsPostBuildCommand \"\")\n"
-        + "set(UnixPreBuildCommand \"\")\n"
-        + "set(UnixPostBuildCommand \"\")\n";
-        this.writeFile(projectName+"/CppExplorerOptions.cmake", fileContents);
+        
+        this.writeFile(projectName+"/CppExplorerOptions.cmake", FileData.projectOptions(projectName, projectType));
     }
 
     deleteFile(filePath: string)
@@ -379,10 +450,14 @@ export class FileSystemInterface
         fs.unlinkSync(filePath);
     }
 
-    private pathExists(p: string): boolean {
-		try {
-			fs.accessSync(p);
-		} catch (err) {
+    private pathExists(path: string): boolean
+    {
+        try
+        {
+			fs.accessSync(path);
+        }
+        catch (err)
+        {
 			return false;
 		}
 
@@ -412,26 +487,7 @@ export class FileSystemInterface
     
     private writeWorkSpaceFile()
     {
-        this.writeFile("CMakeLists.txt", "include(CppExplorerOptions.cmake)\n"
-        + "cmake_minimum_required(VERSION ${MinimumCMakeVersion})\n"
-        + "project("+vscode.workspace.name+" VERSION 0.1.0)\n\n"
-        + "include(CppExplorerDependancies.cmake)\n\n"
-        + "if(${EnableTesting})\n"
-        + "    include(CTest)\n"
-        + "    enable_testing()\n"
-        + "endif()\n\n"
-        + "#Determine if 32 or 64 bit\n"
-        + "set(OSBitness 32)\n"
-        + "if(CMAKE_SIZEOF_VOID_P EQUAL 8)\n"
-        + "    set(OSBitness 64)\n"
-        + "endif()\n\n"
-        + "#Save outputs into bin folder\n"
-        + "set(FullOutputDir \"${CMAKE_SOURCE_DIR}/bin/${CMAKE_SYSTEM_NAME}${OSBitness}/${CMAKE_BUILD_TYPE}\")\n"
-        + "set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY \"${FullOutputDir}/static libs\")\n"
-        + "set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${FullOutputDir})\n"
-        + "set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${FullOutputDir})\n"
-        + "set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${FullOutputDir})\n\n"
-        + "include(CppExplorerProjects.cmake)");
+        this.writeFile("CMakeLists.txt", FileData.workspaceConfig());
     }
 
     makeListFiles(initialPath: string, list: string[], ext: string) : string[]
